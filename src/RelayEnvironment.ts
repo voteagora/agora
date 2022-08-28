086d38d15e221e780be9bcdda7d6650069d43f9f
0xcaff <martincharles07@gmail.com>
2022-08-28 16:16:57 -0500

    add RelayEnvironment.ts

---
+++ src/RelayEnvironment.ts
@@ -0,0 +1,36 @@
+import {
+  Environment,
+  Network,
+  RecordSource,
+  RequestParameters,
+  Store,
+} from "relay-runtime";
+
+async function fetchFn(params: RequestParameters, variables: object) {
+  const response = await fetch(
+    "https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: params.text,
+        variables,
+      }),
+    }
+  );
+
+  return await response.json();
+}
+
+const network = Network.create(fetchFn);
+
+const recordSource = new RecordSource();
+
+const store = new Store(recordSource);
+
+export default new Environment({
+  network,
+  store,
+});
