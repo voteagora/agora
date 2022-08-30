0ad30535edfd31a91a26e7aaee15af972f659f6d
0xcaff <martincharles07@gmail.com>
2022-08-30 16:32:48 -0500

    add icons

---
+++ src/icons/icons.ts
@@ -0,0 +1,11 @@
+import ballot from "./ballot.svg";
+import community from "./community.svg";
+import measure from "./measure.svg";
+import pedestrian from "./pedestrian.svg";
+
+export const icons = {
+  ballot,
+  community,
+  measure,
+  pedestrian,
+};
