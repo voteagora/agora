4f76361ae59683b4a704507d3c2ab841300ac616
0xcaff <martincharles07@gmail.com>
2022-09-01 16:58:21 -0500

    add icons

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -4,8 +4,14 @@ import measure from "./measure.svg";
 import pedestrian from "./pedestrian.svg";
 import discord from "./discord.svg";
 import twitter from "./twitter.svg";
+import piggyBank from "./piggyBank.svg";
+import speakerCone from "./speakerCone.svg";
+import anonNoun from "./anonNoun.svg";
 
 export const icons = {
+  anonNoun,
+  speakerCone,
+  piggyBank,
   discord,
   twitter,
   ballot,
