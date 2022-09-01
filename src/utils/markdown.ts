8baf53383227299b09a535ff4543f23f41136d15
0xcaff <martincharles07@gmail.com>
2022-09-01 11:19:37 -0500

    add markdown util

---
+++ src/utils/markdown.ts
@@ -0,0 +1,20 @@
+import { fromMarkdown } from "mdast-util-from-markdown/dev";
+
+export function getTitleFromProposalDescription(description: string) {
+  const parsed = fromMarkdown(description);
+  const firstChild = parsed.children[0];
+  if (firstChild.type !== "heading") {
+    return null;
+  }
+
+  if (firstChild.children.length !== 1) {
+    return null;
+  }
+
+  const firstTextNode = firstChild.children[0];
+  if (firstTextNode.type !== "text") {
+    return null;
+  }
+
+  return firstTextNode.value;
+}
