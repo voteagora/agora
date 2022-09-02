378ca33e737367ef042ebc66132416797ee65fe6
0xcaff <martincharles07@gmail.com>
2022-09-02 14:39:24 -0500

    add close button

---
+++ src/pages/EditDelegatePage/CloseButton.tsx
@@ -0,0 +1,34 @@
+import { css } from "@emotion/css";
+import * as theme from "../../theme";
+import { CloseIcon } from "../../components/CloseIcon";
+
+type Props = {
+  onClick: () => void;
+};
+
+export function CloseButton({ onClick }: Props) {
+  return (
+    <div
+      onClick={onClick}
+      className={css`
+        border-radius: ${theme.borderRadius.md};
+        cursor: pointer;
+        color: ${theme.colors.gray["500"]};
+        margin: ${theme.spacing["1"]};
+
+        :hover {
+          color: ${theme.colors.gray["600"]};
+          background: ${theme.colors.gray["200"]};
+        }
+      `}
+    >
+      <CloseIcon
+        className={css`
+          width: ${theme.spacing["6"]};
+          height: ${theme.spacing["6"]};
+          margin: ${theme.spacing["1"]};
+        `}
+      />
+    </div>
+  );
+}
