97651c1425bfd2e449fae2f1b089fb7476247416
0xcaff <martincharles07@gmail.com>
2022-09-03 17:43:50 -0500

    fix executable upstream schema

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -1,10 +1,12 @@
+import "isomorphic-fetch";
+
 import { createServer } from "@graphql-yoga/node";
 import { makeGatewaySchema } from "./schema";
 
 async function main() {
   const schema = await makeGatewaySchema();
 
-  const server = createServer({ schema, port: 4001 });
+  const server = createServer({ schema, port: 4001, maskedErrors: false });
   await server.start();
 }
 
