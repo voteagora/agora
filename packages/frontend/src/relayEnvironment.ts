d89aaf5a32bf81e9e2102686acf736844baa191d
0xcaff <martincharles07@gmail.com>
2022-09-03 19:22:35 -0500

    resolve from backend

--- packages/frontend/src/relayEnvironment.ts
+++ packages/frontend/src/relayEnvironment.ts
@@ -13,20 +13,17 @@ export const relayEnvironment = new Environment({
   store,
 });
 
-async function fetchFn(query: string | null, variables?: object) {
-  const response = await fetch(
-    "https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph",
-    {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        query,
-        variables,
-      }),
-    }
-  );
+export async function fetchFn(query: string | null, variables?: object) {
+  const response = await fetch("/graphql", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
 
   return await response.json();
 }
