4ccf21c69e6811cbc9bbd3874e77021970d735f8
0xcaff <martincharles07@gmail.com>
2022-09-04 22:14:04 -0500

    remove markdown dependency
    this thing doesn't play nice with workers or esm

--- packages/backend/src/utils/markdown.ts
+++ packages/backend/src/utils/markdown.ts
@@ -1,20 +1,35 @@
-import { fromMarkdown } from "mdast-util-from-markdown";
+const hashRegex = /^\s*#{1,6}\s+([^\n]+)/;
+const equalTitleRegex = /^\s*([^\n]+)\n(={3,25}|-{3,25})/;
 
+/**
+ * Extract a markdown title from a proposal body that uses the `# Title` format
+ * Returns null if no title found.
+ */
+const extractHashTitle = (body: string) => body.match(hashRegex);
+/**
+ * Extract a markdown title from a proposal body that uses the `Title\n===` format.
+ * Returns null if no title found.
+ */
+const extractEqualTitle = (body: string) => body.match(equalTitleRegex);
+
+/**
+ * Extract title from a proposal's body/description. Returns null if no title found in the first line.
+ * @param body proposal body
+ */
+const extractTitle = (body: string | undefined): string | null => {
+  if (!body) return null;
+  const hashResult = extractHashTitle(body);
+  const equalResult = extractEqualTitle(body);
+  return hashResult ? hashResult[1] : equalResult ? equalResult[1] : null;
+};
+
+const removeBold = (text: string | null): string | null =>
+  text ? text.replace(/\*\*/g, "") : text;
+
+const removeItalics = (text: string | null): string | null =>
+  text ? text.replace(/__/g, "") : text;
+
+// Taken from https://github.com/nounsDAO/nouns-monorepo/blob/0a96001abe99751afa20c41a00adb8e5e32e6fda/packages/nouns-webapp/src/wrappers/nounsDao.ts#L142-L169
 export function getTitleFromProposalDescription(description: string) {
-  const parsed = fromMarkdown(description.slice(0, 500));
-  const firstChild = parsed.children[0];
-  if (firstChild.type !== "heading") {
-    return null;
-  }
-
-  if (firstChild.children.length !== 1) {
-    return null;
-  }
-
-  const firstTextNode = firstChild.children[0];
-  if (firstTextNode.type !== "text") {
-    return null;
-  }
-
-  return firstTextNode.value;
+  return removeItalics(removeBold(extractTitle(description))) ?? "Untitled";
 }
