61c822e376874ea95bb80f094a052af44d214a74
0xcaff <martincharles07@gmail.com>
2022-09-06 23:56:03 -0500

    add page top loading indicator

---
+++ packages/frontend/src/components/RouteTransitionLoadingIndicator.tsx
@@ -0,0 +1,47 @@
+import { useIsNavigationPending } from "./HammockRouter/HammockRouter";
+import React from "react";
+import * as theme from "../theme";
+import { css, keyframes } from "@emotion/css";
+import { motion } from "framer-motion";
+
+const shimmer = keyframes`
+    from {
+      opacity: 0.5;
+    }
+  
+  to {
+    opacity: 0.7;
+  }
+`;
+
+export function RouteTransitionLoadingIndicator() {
+  const isPending = useIsNavigationPending();
+
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: isPending ? 1 : 0,
+      }}
+      transition={{
+        duration: 0.3,
+        delay: 0.3,
+      }}
+    >
+      <div
+        className={css`
+          position: fixed;
+          height: ${theme.spacing["2"]};
+          top: 0;
+          left: 0;
+          right: 0;
+          background: #a0aec0;
+
+          animation: ${shimmer} 0.3s alternate-reverse infinite ease-out;
+        `}
+      />
+    </motion.div>
+  );
+}
