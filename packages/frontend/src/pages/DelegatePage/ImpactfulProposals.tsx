42e4acd03eba353940b169d855f4ceec3bb45948
0xcaff <martincharles07@gmail.com>
2022-09-09 14:00:20 -0500

    normalize colors a bit

--- packages/frontend/src/pages/DelegatePage/ImpactfulProposals.tsx
+++ packages/frontend/src/pages/DelegatePage/ImpactfulProposals.tsx
@@ -45,7 +45,7 @@ export function ImpactfulProposals({ fragment }: Props) {
           border: 1px solid ${theme.colors.gray.eb};
           box-shadow: ${theme.boxShadow.newDefault};
           padding: ${theme.spacing["4"]};
-          background-color: white;
+          background-color: ${theme.colors.white};
         `}
       >
         <HStack>
