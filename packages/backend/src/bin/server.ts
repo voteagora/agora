37aaa2531fc9de4ec5b0d42a6f9c627439297441
Martin Charles <martincharles07@gmail.com>
2022-09-11 14:48:17 -0500

    hook up persistence (#5)

--- packages/backend/src/bin/server.ts
+++ packages/backend/src/bin/server.ts
@@ -4,19 +4,44 @@ import { makeGatewaySchema } from "../schema";
 import { useTiming } from "@envelop/core";
 import { createInMemoryCache } from "@envelop/response-cache";
 import { makeCachePlugin } from "../cache";
+import { AgoraContextType, StatementStorage, StoredStatement } from "../model";
+import { presetDelegateStatements } from "../presetStatements";
 
 async function main() {
-  const schema = await makeGatewaySchema();
+  const delegateStatements = new Map(presetDelegateStatements);
+  const schema = makeGatewaySchema();
 
   const cache = createInMemoryCache();
+  const context: AgoraContextType = {
+    statementStorage: makeStatementStorageFromMap(delegateStatements),
+  };
 
   const server = createServer({
     schema,
+    context,
     port: 4001,
     maskedErrors: false,
     plugins: [useTiming(), makeCachePlugin(cache)],
   });
   await server.start();
+}
+
+function makeStatementStorageFromMap(
+  delegateStatements: Map<string, StoredStatement>
+): StatementStorage {
+  return {
+    async getStatement(address: string): Promise<StoredStatement> {
+      return delegateStatements.get(address.toLowerCase()) ?? null;
+    },
+    async addStatement(statement: StoredStatement): Promise<void> {
+      delegateStatements.set(statement.address.toLowerCase(), statement);
+    },
+    async listStatements(): Promise<string[]> {
+      return Array.from(delegateStatements.keys()).map((it) =>
+        it.toLowerCase()
+      );
+    },
+  };
 }
 
 main();
