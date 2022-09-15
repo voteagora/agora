585b396f2f2af3c1a5a720a0a29a95216f2141c7
Martin Charles <martincharles07@gmail.com>
2022-09-15 10:18:35 -0500

    add sentry to frontend (#11)

---
+++ packages/frontend/src/sentry.ts
@@ -0,0 +1,13 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  release: process.env.REACT_APP_GITHUB_SHA,
+  environment: process.env.REACT_APP_DEPLOY_ENV,
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
