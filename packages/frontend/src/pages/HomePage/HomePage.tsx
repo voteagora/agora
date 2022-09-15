11e60467a5af2ab3bfc68fcfb86237eee1cbc7fa
0xcaff <martincharles07@gmail.com>
2022-09-14 23:40:00 -0500

    add most relevant sort to client

--- packages/frontend/src/pages/HomePage/HomePage.tsx
+++ packages/frontend/src/pages/HomePage/HomePage.tsx
@@ -32,7 +32,7 @@ export function locationToVariables(location: Location) {
     orderBy:
       orderByValidValues.find(
         (needle) => needle === location.search["orderBy"]
-      ) ?? "mostNounsRepresented",
+      ) ?? "mostRelevant",
   };
 }
 
