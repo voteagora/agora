0392dc53d0a4d94925354d3a7c19a1172cddc5b6
Martin Charles <martincharles07@gmail.com>
2022-10-17 14:14:03 -0500

    dynamic opengraph images (#15)

---
+++ packages/render-opengraph/src/main.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use graphql_client::{GraphQLQuery, Response};
+use reqwest::Client;
+use std::fs::File;
+use std::io::Write;
+use text_test::{
+    draw_opengraph_image_inner, open_graph_render_query, DrawDependencies, OpenGraphRenderQuery,
+};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let request_body = OpenGraphRenderQuery::build_query(open_graph_render_query::Variables {
+        address: "noun22.⌐◨-◨".to_string(),
+    });
+
+    let client = Client::new();
+    let res = client
+        .post("https://nounsagora.com/graphql")
+        .json(&request_body)
+        .send()
+        .await?;
+    let response_body: Response<open_graph_render_query::ResponseData> = res.json().await?;
+
+    let draw_dependencies = DrawDependencies::create_inner(
+        include_str!("../../../node_modules/@nouns/assets/dist/image-data.json",),
+        include_bytes!("../resources/DejaVuSans.ttf") as &[u8],
+        include_bytes!("../resources/DejaVuSans-Bold.ttf") as &[u8],
+    )?;
+
+    let image =
+        draw_opengraph_image_inner(&draw_dependencies, response_body.data.unwrap())?.unwrap();
+
+    File::create("./test.png")?.write_all(&image)?;
+
+    Ok(())
+}
---
+++ packages/render-opengraph/src/text.rs
@@ -0,0 +1,84 @@
+use fontdue::layout::{
+    CoordinateSystem, HorizontalAlign, Layout, LayoutSettings, TextStyle, WrapStyle,
+};
+use fontdue::Font;
+use raqote::Image;
+use raqote::{AntialiasMode, BlendMode, DrawOptions, DrawTarget};
+
+pub fn draw_text_layout(
+    dt: &mut DrawTarget,
+    max_text_width: f32,
+    layout: &mut Layout,
+    fonts: &[Font],
+) {
+    // dt.fill_rect(
+    //     0f32,
+    //     0f32,
+    //     max_text_width,
+    //     layout.height(),
+    //     &Source::Solid(SolidSource {
+    //         r: 255,
+    //         g: 0,
+    //         b: 0,
+    //         a: 255,
+    //     }),
+    //     &DrawOptions::default(),
+    // );
+
+    layout.glyphs().iter().for_each(|glyph| {
+        let font = &fonts[glyph.font_index];
+        let (_metrics, bitmap) = font.rasterize_config(glyph.key);
+
+        let data = bitmap
+            .into_iter()
+            .map(|value| u32::from_le_bytes([0, 0, 0, value]))
+            .collect::<Vec<u32>>();
+
+        dt.draw_image_at(
+            glyph.x,
+            glyph.y,
+            &Image {
+                width: glyph.width as i32,
+                height: glyph.height as i32,
+                data: &data,
+            },
+            &DrawOptions {
+                blend_mode: BlendMode::SrcOver,
+                antialias: AntialiasMode::Gray,
+                alpha: 1.0,
+            },
+        );
+    });
+}
+
+pub fn largest_text_layout_fitting(
+    max_width: f32,
+    text: &str,
+    max_font_size: u32,
+    min_font_size: u32,
+    fonts: &[Font],
+) -> Layout {
+    let mut layout = Layout::new(CoordinateSystem::PositiveYDown);
+
+    layout.reset(&LayoutSettings {
+        x: 0.0,
+        y: 0.0,
+        max_width: Some(max_width),
+        max_height: None,
+        horizontal_align: HorizontalAlign::Left,
+        wrap_style: WrapStyle::Letter,
+        ..LayoutSettings::default()
+    });
+
+    for font_size in (min_font_size..=(max_font_size)).rev() {
+        layout.append(fonts, &TextStyle::new(text, font_size as f32, 1));
+
+        if layout.lines().map(|lines| lines.len()).unwrap_or(0) == 1 {
+            break;
+        } else {
+            layout.clear()
+        }
+    }
+
+    layout
+}
