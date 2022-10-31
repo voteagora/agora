e1bb41f078a55509e2f0fe066912e05fa5a95e97
0xcaff <martincharles07@gmail.com>
2022-10-31 18:21:12 -0500

    remove discord
    add it back separately later

--- packages/backend/src/worker/scheduled.ts
+++ packages/backend/src/worker/scheduled.ts
@@ -1,20 +1,13 @@
 import Toucan from "toucan-js";
 import { Env } from "./env";
 import { ethers } from "ethers";
-import { NNSENSReverseResolver__factory } from "../contracts/generated";
 import { parseStorage, updateSnapshot } from "../snapshot";
 import { loadSnapshot, updateLatestSnapshot } from "./snapshot";
-import { postDiscordMessagesSinceLastUpdate } from "../discord";
 
 export async function scheduled(cron: string, env: Env, sentry: Toucan) {
   const provider = new ethers.providers.AlchemyProvider(
     "mainnet",
     env.ALCHEMY_API_KEY
-  );
-
-  const resolver = NNSENSReverseResolver__factory.connect(
-    "0x5982cE3554B18a5CF02169049e81ec43BFB73961",
-    provider
   );
 
   await Promise.all([
@@ -25,35 +18,10 @@ export async function scheduled(cron: string, env: Env, sentry: Toucan) {
 
       const oldSnapshot = await loadSnapshot(env);
 
-      const nextSnapshot = await updateSnapshot(
-        sentry,
-        provider,
-        resolver,
-        oldSnapshot
-      );
+      const nextSnapshot = await updateSnapshot(sentry, provider, oldSnapshot);
 
       await env.INDEXER.put("snapshot.json", JSON.stringify(nextSnapshot));
       updateLatestSnapshot(parseStorage(nextSnapshot));
-    })(),
-    await (async () => {
-      if (!env.DISCORD_WEBHOOK_URL) {
-        return;
-      }
-
-      const lastBlock = await env.LAST_BLOCKED_FETCHED.get("block");
-
-      const latestBlock = await provider.getBlockNumber();
-      const latestSafeBlock = latestBlock - 10;
-
-      await postDiscordMessagesSinceLastUpdate(
-        provider,
-        resolver,
-        lastBlock ? parseInt(lastBlock) + 1 : latestSafeBlock - 1000,
-        latestSafeBlock,
-        env.DISCORD_WEBHOOK_URL
-      );
-
-      await env.LAST_BLOCKED_FETCHED.put("block", latestSafeBlock.toString());
     })(),
   ]);
 }
