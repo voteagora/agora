d3a89656115fbf34d5ef73ec8564fc1434b1f165
0xcaff <martincharles07@gmail.com>
2022-10-31 22:24:59 -0500

    fix reverse resolution

--- packages/backend/src/utils/resolveName.ts
+++ packages/backend/src/utils/resolveName.ts
@@ -1,21 +1,60 @@
 import { ethers } from "ethers";
 import {
+  ENSNameResolver__factory,
   ENSAddressResolver__factory,
   ENSRegistryWithFallback,
   ENSRegistryWithFallback__factory,
 } from "../contracts/generated";
 
+function makeEnsRegistry(provider: ethers.providers.Provider) {
+  return ENSRegistryWithFallback__factory.connect(
+    "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+    provider
+  );
+}
+
+async function resolveAddress(
+  address: string,
+  provider: ethers.providers.Provider
+) {
+  const registry = makeEnsRegistry(provider);
+
+  const reverseRecordName =
+    address.toLowerCase().replace("0x", "") + ".addr.reverse";
+
+  const resolverAddress = await getResolver(reverseRecordName, registry);
+  if (!resolverAddress) {
+    return null;
+  }
+
+  const resolver = ENSNameResolver__factory.connect(resolverAddress, provider);
+
+  const hash = ethers.utils.namehash(reverseRecordName);
+  return await resolver.name(hash);
+}
+
+export async function resolveNameFromAddress(
+  address: string,
+  provider: ethers.providers.Provider
+) {
+  const resolvedName = await resolveAddress(address, provider);
+  if (!resolvedName) {
+    return null;
+  }
+
+  const resolvedAddress = await resolveEnsName(resolvedName, provider);
+  if (resolvedAddress.toLowerCase() !== address.toLowerCase()) {
+    return null;
+  }
+
+  return resolvedName;
+}
+
 export async function resolveEnsName(
   name: string,
   provider: ethers.providers.Provider
 ) {
-  return resolveName(
-    name,
-    ENSRegistryWithFallback__factory.connect(
-      "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
-      provider
-    )
-  );
+  return resolveName(name, makeEnsRegistry(provider));
 }
 
 async function resolveName(
