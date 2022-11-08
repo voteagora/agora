95240f79f531673a60e98d5cb548bdd42038d4c1
0xcaff <martincharles07@gmail.com>
2022-11-07 20:17:50 -0600

    add subgraph
    this is a backup option given the dynamodb consistency issues

--- packages/subgraph/src/ens-token.ts
+++ packages/subgraph/src/ens-token.ts
@@ -4,15 +4,8 @@ import {
   DelegateVotesChanged,
   Transfer,
 } from "../generated/ENSToken/ENSToken";
-import { Account, Block, Metrics, Proposal, Vote } from "../generated/schema";
-import {
-  ProposalCanceled,
-  ProposalCreated,
-  ProposalExecuted,
-  ProposalQueued,
-  QuorumNumeratorUpdated,
-  VoteCast,
-} from "../generated/ENSGovernor/ENSGovernor";
+import { Account } from "../generated/schema";
+import { getMetrics } from "./metrics";
 
 export function handleDelegateVotesChanged(event: DelegateVotesChanged): void {
   const account = getOrCreateAccount(event.params.delegate);
@@ -87,77 +80,6 @@ export function handleTransfer(event: Transfer): void {
   metrics.save();
 }
 
-export function handleProposalCreated(event: ProposalCreated): void {
-  const proposal = new Proposal(event.params.proposalId.toString());
-
-  proposal.proposer = event.params.proposer.toHex();
-  proposal.startBlock = event.params.startBlock;
-  proposal.endBlock = event.params.endBlock;
-  proposal.description = event.params.description;
-  proposal.targets = event.params.targets.map<string>((it) => it.toHex());
-  proposal.values = event.params.values;
-  proposal.signatures = event.params.signatures;
-  proposal.calldatas = event.params.calldatas;
-
-  proposal.status = "CREATED";
-
-  proposal.save();
-}
-
-export function handleVoteCast(event: VoteCast): void {
-  const transactionHash = event.transaction.hash.toHex();
-  const blockHash = event.block.hash.toHex();
-
-  const block = new Block(blockHash);
-  block.timestamp = block.timestamp;
-  block.save();
-
-  const vote = new Vote(transactionHash);
-  vote.block = blockHash;
-  vote.proposal = event.params.proposalId.toString();
-  vote.voter = event.params.voter.toHex();
-  vote.support = event.params.support;
-  vote.weight = event.params.weight;
-  vote.reason = event.params.reason;
-  vote.save();
-}
-
-export function handleProposalCanceled(event: ProposalCanceled): void {
-  const proposal = mustGetProposal(event.params.proposalId);
-  proposal.status = "CANCELLED";
-  proposal.save();
-}
-
-export function handleProposalExecuted(event: ProposalExecuted): void {
-  const proposal = mustGetProposal(event.params.proposalId);
-  proposal.status = "EXECUTED";
-  proposal.save();
-}
-
-export function handleProposalQueued(event: ProposalQueued): void {
-  const proposal = mustGetProposal(event.params.proposalId);
-  proposal.status = "QUEUED";
-  proposal.activatedAt = event.params.eta;
-  proposal.save();
-}
-
-export function handleQuorumNumeratorUpdated(
-  event: QuorumNumeratorUpdated
-): void {
-  const metrics = getMetrics();
-  metrics.quorumNumerator = event.params.newQuorumNumerator;
-  metrics.save();
-}
-
-function mustGetProposal(proposalId: BigInt): Proposal {
-  const proposal = Proposal.load(proposalId.toString());
-  if (!proposal) {
-    throw new Error("could not find proposal");
-  }
-
-  return proposal;
-}
-
 function getOrCreateAccount(address: Address): Account {
   const loadedAccount = Account.load(address.toHex());
   if (loadedAccount) {
@@ -168,17 +90,4 @@ function getOrCreateAccount(address: Address): Account {
   account.tokensRepresented = BigInt.zero();
   account.amountOwned = BigInt.zero();
   return account;
-}
-
-function getMetrics(): Metrics {
-  const loaded = Metrics.load("METRICS");
-  if (loaded) {
-    return loaded;
-  }
-
-  const newMetrics = new Metrics("METRICS");
-  newMetrics.totalSupply = BigInt.zero();
-  newMetrics.quorumNumerator = BigInt.zero();
-  newMetrics.delegatedSupply = BigInt.zero();
-  return newMetrics;
 }
