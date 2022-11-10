b7bafd2d76810fc829ccf57e6ce9fe3b54533ad2
0xcaff <martincharles07@gmail.com>
2022-11-09 19:03:16 -0600

    Add snapshot votes

--- packages/backend/src/bin/server.ts
+++ packages/backend/src/bin/server.ts
@@ -12,69 +12,21 @@ import {
   makeNoOpCache,
 } from "../utils/cache";
 import { useApolloTracing } from "@envelop/apollo-tracing";
-import { promises as fs } from "fs";
-import { initialSnapshot } from "../snapshot";
-import { z } from "zod";
+import { parseStorage } from "../snapshot";
 import { makeDynamoStatementStorage } from "../store/dynamo/statement";
 import { makeDynamoDelegateStore } from "../store/dynamo/delegates";
 import { ethers } from "ethers";
 import { TransparentMultiCallProvider } from "../multicall";
-
-const snapshotVoteSchema = z.array(
-  z.object({
-    choice: z.array(z.number()).or(z.number()),
-    created: z.number(),
-    id: z.string(),
-    reason: z.string(),
-    voter: z.string(),
-    proposal: z.object({
-      id: z.string(),
-    }),
-  })
-);
-
-const snapshotProposalsSchema = z.object({
-  proposals: z.array(
-    z.object({
-      id: z.string(),
-      title: z.string(),
-      link: z.string(),
-      choices: z.array(z.string()),
-      scores: z.array(z.number()),
-    })
-  ),
-});
-
-export async function getSnapshotVotes() {
-  const votes = snapshotVoteSchema.parse(
-    JSON.parse(
-      await fs.readFile("./data/snapshot/ens.eth/votes.json", {
-        encoding: "utf-8",
-      })
-    )
-  );
-
-  const proposals = snapshotProposalsSchema.parse(
-    JSON.parse(
-      await fs.readFile("./data/snapshot/ens.eth/proposals.json", {
-        encoding: "utf-8",
-      })
-    )
-  );
-
-  return {
-    votes,
-    proposals: proposals.proposals,
-  };
-}
+import { makeSnapshotVoteStorage } from "../store/dynamo/snapshotVotes";
+import { promises as fs } from "fs";
 
 async function main() {
   const schema = makeGatewaySchema();
   const baseProvider = new ethers.providers.CloudflareProvider();
 
-  const snapshot = await initialSnapshot();
-
-  const snapshotVotes = await getSnapshotVotes();
+  const snapshot = parseStorage(
+    JSON.parse(await fs.readFile("snapshot.json", { encoding: "utf-8" }))
+  );
 
   const dynamoDb = new DynamoDB({});
 
@@ -86,33 +38,7 @@ async function main() {
       return {
         provider,
         snapshot,
-        snapshotVoteStorage: {
-          async getSnapshotVotesByVoter(
-            address: string
-          ): Promise<SnapshotVote[]> {
-            return snapshotVotes.votes
-              .filter((it) => it.voter.toLowerCase() === address.toLowerCase())
-              .map((vote) => {
-                const proposal = snapshotVotes.proposals.find(
-                  (it) => it.id === vote.proposal.id
-                );
-
-                return {
-                  id: vote.id.toString(),
-                  reason: vote.reason,
-                  created: vote.created,
-                  proposal: {
-                    id: proposal.id,
-                    scores: proposal.scores,
-                    choices: proposal.choices,
-                    title: proposal.title,
-                    link: proposal.link,
-                  },
-                  choice: vote.choice as any,
-                };
-              });
-          },
-        },
+        snapshotVoteStorage: makeSnapshotVoteStorage(dynamoDb),
         delegateStorage: makeDynamoDelegateStore(dynamoDb),
         statementStorage: makeDynamoStatementStorage(dynamoDb),
 
