dbfac408140f3f518ad5e6491278470ceb95a12b
0xcaff <martincharles07@gmail.com>
2022-11-10 17:08:26 -0600

    fix dependencies

--- packages/backend/src/worker/env.ts
+++ packages/backend/src/worker/env.ts
@@ -1,27 +1,14 @@
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
   ENVIRONMENT: "prod" | "dev" | "staging";
   DEPLOYMENT: string;
   SENTRY_DSN: string;
   ALCHEMY_API_KEY: string;
   GITHUB_SHA: string;
 
-  STATEMENTS: KVNamespace;
-  APPLICATION_CACHE: KVNamespace;
   EMAILS: KVNamespace;
   INDEXER: KVNamespace;
   __STATIC_CONTENT: KVNamespace;
 
   AWS_ACCESS_KEY_ID: string;
   AWS_SECRET_ACCESS_KEY: string;
-
-  DISCORD_WEBHOOK_URL: string;
-  LAST_BLOCKED_FETCHED: KVNamespace;
 }
