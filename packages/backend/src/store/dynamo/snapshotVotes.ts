968724e101f38aec2d7553f1aab30691d95ed03a
0xcaff <martincharles07@gmail.com>
2022-11-09 19:03:16 -0600

    Add snapshot votes

---
+++ packages/backend/src/store/dynamo/snapshotVotes.ts
@@ -0,0 +1,29 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { SnapshotVote, SnapshotVoteStorage } from "../../model";
+import { marshaller, TableName, withAttributes } from "./utils";
+import {
+  equals,
+  ExpressionAttributes,
+  serializeConditionExpression,
+} from "@aws/dynamodb-expressions";
+
+export function makeSnapshotVoteStorage(dynamo: DynamoDB): SnapshotVoteStorage {
+  return {
+    async getSnapshotVotesByVoter(address: string): Promise<SnapshotVote[]> {
+      const expressionAttributes = new ExpressionAttributes();
+      const result = await dynamo.query({
+        TableName,
+        KeyConditionExpression: serializeConditionExpression(
+          {
+            subject: "PartitionKey",
+            ...equals(`SnapshotVote#${address.toLowerCase()}`),
+          },
+          expressionAttributes
+        ),
+        ...withAttributes(expressionAttributes),
+      });
+
+      return result.Items.map((it) => marshaller.unmarshallItem(it)) as any;
+    },
+  };
+}
