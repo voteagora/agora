981ddf7891a220cd5e2d67d1b185dbdea8d25c7d
0xcaff <martincharles07@gmail.com>
2022-11-11 12:13:01 -0600

    add cname-worker

---
+++ packages/cname-worker/src/index.ts
@@ -0,0 +1,45 @@
+type ServiceBinding = {
+  fetch(request: Request): Promise<Response>;
+};
+
+export interface Env extends ServiceBindings {
+  ENVIRONMENT: "dev" | "prod";
+}
+
+type ServiceBindings = {
+  ENS: ServiceBinding;
+  NOUNS: ServiceBinding;
+  LIL_NOUNS: ServiceBinding;
+};
+
+type MappingType = {
+  [host: string]: keyof ServiceBindings;
+};
+
+const dev: MappingType = {};
+
+const prod: MappingType = {
+  "agora.ensdao.xyz": "ENS",
+};
+
+const mapping: { dev: MappingType; prod: MappingType } = {
+  dev,
+  prod,
+};
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    const resolvedBinding = mapping[env.ENVIRONMENT][url.host];
+    if (!resolvedBinding) {
+      return new Response("not found", { status: 404 });
+    }
+
+    const serviceBinding = env[resolvedBinding];
+    return await serviceBinding.fetch(request);
+  },
+};
