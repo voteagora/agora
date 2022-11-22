8595fabbe58b9fb2803515cc317de63bed8dc28f
martin <martincharles07@gmail.com>
2022-11-21 16:50:48 -0800

    lazy load query

--- packages/frontend/src/pages/EditDelegatePage/EditDelegatePageRoute.tsx
+++ packages/frontend/src/pages/EditDelegatePage/EditDelegatePageRoute.tsx
@@ -1,26 +1,8 @@
-import graphql from "babel-plugin-relay/macro";
 import { RouteLoadingParams } from "../../components/HammockRouter/HammockRouter";
 import React from "react";
 import { EditDelegatePageRouteQuery } from "./__generated__/EditDelegatePageRouteQuery.graphql";
 
-export const editDelegateQuery = graphql`
-  query EditDelegatePageRouteQuery($address: String!) {
-    ...DelegateStatementFormFragment @arguments(address: $address)
-
-    delegate(addressOrEnsName: $address) {
-      ...VoterPanelFragment
-    }
-  }
-`;
-
 export const editDelegatePageRoute: RouteLoadingParams<EditDelegatePageRouteQuery> =
   {
-    query: editDelegateQuery,
     element: React.lazy(() => import("./EditDelegatePage")),
-    variablesFromLocation(location) {
-      return {
-        // todo: value?
-        address: "",
-      };
-    },
   };
