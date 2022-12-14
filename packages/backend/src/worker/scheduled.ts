91c7d99b6b1b3160d034da83cb1e38abc947d464
martin <martincharles07@gmail.com>
2022-12-14 17:17:07 -0500

    remove updates from cloudflare

--- packages/backend/src/worker/scheduled.ts
+++ packages/backend/src/worker/scheduled.ts
@@ -1,26 +1,4 @@
 import Toucan from "toucan-js";
 import { Env } from "./env";
-import { ethers } from "ethers";
-import { updateSnapshot } from "../snapshot";
-import { loadSnapshot, writeSnapshot } from "./snapshot";
 
-export async function scheduled(cron: string, env: Env, sentry: Toucan) {
-  const provider = new ethers.providers.AlchemyProvider(
-    "mainnet",
-    env.ALCHEMY_API_KEY
-  );
-
-  await Promise.all([
-    await (async () => {
-      if (cron !== "0 * * * *") {
-        return;
-      }
-
-      const oldSnapshot = await loadSnapshot(env);
-
-      const nextSnapshot = await updateSnapshot(sentry, provider, oldSnapshot);
-
-      await writeSnapshot(env, nextSnapshot);
-    })(),
-  ]);
-}
+export async function scheduled(cron: string, env: Env, sentry: Toucan) {}
