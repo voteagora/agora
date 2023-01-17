c6005d7f199a9b3df0008732979f3abfe9c4116b
martin <martincharles07@gmail.com>
2023-01-16 19:46:00 -0600

    add subgraph

---
+++ packages/subgraph/tests/governance-token-utils.ts
@@ -0,0 +1,134 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  DelegateChanged,
+  DelegateVotesChanged,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/GovernanceToken/GovernanceToken"
+
+export function createApprovalEvent(
+  owner: Address,
+  spender: Address,
+  value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return approvalEvent
+}
+
+export function createDelegateChangedEvent(
+  delegator: Address,
+  fromDelegate: Address,
+  toDelegate: Address
+): DelegateChanged {
+  let delegateChangedEvent = changetype<DelegateChanged>(newMockEvent())
+
+  delegateChangedEvent.parameters = new Array()
+
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam("delegator", ethereum.Value.fromAddress(delegator))
+  )
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "fromDelegate",
+      ethereum.Value.fromAddress(fromDelegate)
+    )
+  )
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "toDelegate",
+      ethereum.Value.fromAddress(toDelegate)
+    )
+  )
+
+  return delegateChangedEvent
+}
+
+export function createDelegateVotesChangedEvent(
+  delegate: Address,
+  previousBalance: BigInt,
+  newBalance: BigInt
+): DelegateVotesChanged {
+  let delegateVotesChangedEvent = changetype<DelegateVotesChanged>(
+    newMockEvent()
+  )
+
+  delegateVotesChangedEvent.parameters = new Array()
+
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam("delegate", ethereum.Value.fromAddress(delegate))
+  )
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousBalance",
+      ethereum.Value.fromUnsignedBigInt(previousBalance)
+    )
+  )
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newBalance",
+      ethereum.Value.fromUnsignedBigInt(newBalance)
+    )
+  )
+
+  return delegateVotesChangedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
