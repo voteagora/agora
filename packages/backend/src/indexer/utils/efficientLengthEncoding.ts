080cee7e574fa36d43a833fe854aa1ca9c223ef8
martin <martincharles07@gmail.com>
2023-01-23 16:07:17 -0600

    clean up indexers
    sort correctly

--- packages/backend/src/indexer/utils/efficientLengthEncoding.ts
+++ packages/backend/src/indexer/utils/efficientLengthEncoding.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 
-const LENGTH_MARKER = ";";
+const LENGTH_MARKER = "=";
 
 export function efficientLengthEncodingNaturalPositiveNumbers(
   num: ethers.BigNumber
@@ -15,5 +15,42 @@ export function efficientLengthEncodingNaturalPositiveNumbers(
         )
       : "",
     encoded,
+  ].join("");
+}
+
+export function efficientLengthEncodingNaturalNumbers(num: ethers.BigNumber) {
+  const isNegative = num.lt(0);
+
+  return [
+    ...(() => {
+      const encoded = efficientLengthEncodingNaturalPositiveNumbers(num.abs());
+
+      if (!isNegative) {
+        return encoded;
+      }
+
+      return encoded.split("").map((it, idx) => {
+        switch (it) {
+          case LENGTH_MARKER: {
+            return "-";
+          }
+
+          case "0":
+          case "1":
+          case "2":
+          case "3":
+          case "4":
+          case "5":
+          case "6":
+          case "7":
+          case "8":
+          case "9":
+            return 9 - Number(it);
+
+          default:
+            throw new Error(`unknown character ${it}`);
+        }
+      });
+    })(),
   ].join("");
 }
