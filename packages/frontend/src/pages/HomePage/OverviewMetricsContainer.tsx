840b5c0e2ade267adc750f347cf638a82fe4978e
martin <martincharles07@gmail.com>
2023-01-24 01:04:07 -0600

    fix z-index

--- packages/frontend/src/pages/HomePage/OverviewMetricsContainer.tsx
+++ packages/frontend/src/pages/HomePage/OverviewMetricsContainer.tsx
@@ -57,7 +57,7 @@ export function OverviewMetricsContainer({ fragmentRef }: Props) {
         max-width: ${theme.maxWidth["6xl"]};
         width: 100%;
         flex-wrap: wrap;
-        z-index:10;
+        z-index: 1;
 
         padding-left: ${theme.spacing["4"]};
         padding-right: ${theme.spacing["4"]};
