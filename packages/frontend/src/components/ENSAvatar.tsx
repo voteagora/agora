6e9c5e1375e5d968bbdd2fe5ed94fd87ea4d2a88
martin <martincharles07@gmail.com>
2023-01-26 00:20:28 -0600

    fix ens avatar

--- packages/frontend/src/components/ENSAvatar.tsx
+++ packages/frontend/src/components/ENSAvatar.tsx
@@ -3,12 +3,40 @@ import graphql from "babel-plugin-relay/macro";
 import { useFragment } from "react-relay";
 import { ENSAvatarFragment$key } from "./__generated__/ENSAvatarFragment.graphql";
 import { SuspenseImage } from "./SuspenseImage/SuspenseImage";
-import { useEnsAvatar, useProvider } from "wagmi";
+import { useEnsAvatar } from "wagmi";
 import { AvatarResolver } from "@ensdomains/ens-avatar";
+import { createContext, ReactNode, useContext, useMemo } from "react";
+import { ethers } from "ethers";
+import { TransparentMultiCallProvider } from "../multicallProvider";
 
 type Props = {
   fragment: ENSAvatarFragment$key;
   className: string;
+};
+
+const ENSAvatarContext = createContext<TransparentMultiCallProvider | null>(
+  null
+);
+
+type ProviderProps = {
+  children: ReactNode;
+};
+
+export const ENSAvatarProvider = ({ children }: ProviderProps) => {
+  const provider = useMemo(() => {
+    const provider = new ethers.providers.AlchemyProvider(
+      "mainnet",
+      process.env.REACT_APP_ALCHEMY_ID
+    );
+
+    return new TransparentMultiCallProvider(provider);
+  }, []);
+
+  return (
+    <ENSAvatarContext.Provider value={provider}>
+      {children}
+    </ENSAvatarContext.Provider>
+  );
 };
 
 export function ENSAvatar({ fragment, className }: Props) {
@@ -21,7 +49,10 @@ export function ENSAvatar({ fragment, className }: Props) {
     `,
     fragment
   );
-  const provider = useProvider();
+  const provider = useContext(ENSAvatarContext);
+  if (!provider) {
+    throw new Error("missing provider");
+  }
 
   const url = useQuery(
     ["ENSAvatar", name],
@@ -40,5 +71,7 @@ export function ENSAvatar({ fragment, className }: Props) {
     }
   );
 
-  return <SuspenseImage src={url.data ?? null} className={className} name={name} />;
+  return (
+    <SuspenseImage src={url.data ?? null} className={className} name={name} />
+  );
 }
