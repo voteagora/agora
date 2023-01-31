b1c3055d0faea5912bc7820ef6d7547524d0d5fb
martin <martincharles07@gmail.com>
2023-01-31 14:40:25 -0600

    fix honeycomb

--- packages/backend/src/worker/index.ts
+++ packages/backend/src/worker/index.ts
@@ -1,8 +1,12 @@
 import { makeToucanOptions, wrapModuleSentry } from "./sentry";
 import { Env } from "./env";
-import { wrapModule } from "@cloudflare/workers-honeycomb-logger";
+import {
+  wrapModule,
+  wrapDurableObject,
+} from "@cloudflare/workers-honeycomb-logger";
 import { scheduled } from "./scheduled";
 import { fetch } from "./fetch";
+import { StorageDurableObjectV1 as StorageDurableObjectV1Implementation } from "./durableObject";
 
 const sentryWrappedModule = wrapModuleSentry(makeToucanOptions, (sentry) => ({
   async fetch(request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -19,4 +23,7 @@ export default {
   scheduled: sentryWrappedModule.scheduled,
 };
 
-export { StorageDurableObjectV1 } from "./durableObject";
+export const StorageDurableObjectV1 = wrapDurableObject(
+  {},
+  StorageDurableObjectV1Implementation as any
+);
