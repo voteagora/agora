a2e78c786c03b08f554ad05eb564f6d95820627a
Yitong Zhang <zha.yitong@gmail.com>
2023-01-31 19:29:26 -0600

    updated metrics section

--- packages/frontend/src/pages/HomePage/OverviewMetricsContainer.tsx
+++ packages/frontend/src/pages/HomePage/OverviewMetricsContainer.tsx
@@ -8,7 +8,8 @@ import graphql from "babel-plugin-relay/macro";
 import { OverviewMetricsContainerFragment$key } from "./__generated__/OverviewMetricsContainerFragment.graphql";
 import { TokenAmountDisplay } from "../../components/TokenAmountDisplay";
 import { BigNumber } from "ethers";
-import { bpsToString } from "../../utils/bps";
+import { CalendarIcon } from "@heroicons/react/20/solid";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 
 type Props = {
   fragmentRef: OverviewMetricsContainerFragment$key;
@@ -78,37 +79,177 @@ export function OverviewMetricsContainer({ fragmentRef }: Props) {
         body={
           <>
             <TokenAmountDisplay fragment={metrics.delegatedSupply} /> /{" "}
-            <TokenAmountDisplay fragment={metrics.totalSupply} /> (
-            {BigNumber.from(metrics.delegatedSupply.amount)
-              .mul(100)
-              .div(metrics.totalSupply.amount)
-              .toNumber()}
-            % delegation)
+            <TokenAmountDisplay fragment={metrics.totalSupply} />
           </>
         }
       />
+      <HStack
+        gap="3"
+        className={css`
+          background: ${theme.colors.white};
+          border-radius: ${theme.spacing["3"]};
+          padding: ${theme.spacing["3"]};
+          border-width: ${theme.spacing.px};
+          border-color: ${theme.colors.gray["300"]};
+          box-shadow: ${theme.boxShadow.newDefault};
+        `}
+      >
+        <div
+          className={css`
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: ${theme.spacing["2"]};
+            border-width: ${theme.spacing.px};
+            border-color: ${theme.colors.gray["300"]};
+            background: ${color};
+            flex-shrink: 0;
+            padding: ${theme.spacing["3"]};
+          `}
+        >
+          <img
+            className={css`
+              width: 24px;
+              height: 24px;
+            `}
+            src={icons.ballot}
+            alt={icons.ballot}
+          />
+        </div>
+        <VStack>
+          <div
+            className={css`
+              font-size: ${theme.fontSize.sm};
+              color: ${theme.colors.gray["700"]};
+            `}
+          >
+            Quorum
+          </div>
+          <HStack gap="2" alignItems="center">
+            <div>30% of delegated tokens </div>
+          </HStack>
+        </VStack>
+      </HStack>
+      <a
+        href="https://github.com/ethereum-optimism/OPerating-manual/blob/main/manual.md"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <HStack
+          gap="3"
+          className={css`
+            background: ${theme.colors.white};
+            border-radius: ${theme.spacing["3"]};
+            padding: ${theme.spacing["3"]};
+            border-width: ${theme.spacing.px};
+            border-color: ${theme.colors.gray["300"]};
+            box-shadow: ${theme.boxShadow.newDefault};
+          `}
+        >
+          <div
+            className={css`
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              border-radius: ${theme.spacing["2"]};
+              border-width: ${theme.spacing.px};
+              border-color: ${theme.colors.gray["300"]};
+              background: ${color};
+              flex-shrink: 0;
+              padding: ${theme.spacing["3"]};
+            `}
+          >
+            <img
+              className={css`
+                width: 24px;
+                height: 24px;
+              `}
+              src={icons.ballot}
+              alt={icons.ballot}
+            />
+          </div>
+          <VStack>
+            <div
+              className={css`
+                font-size: ${theme.fontSize.sm};
+                color: ${theme.colors.gray["700"]};
+              `}
+            >
+              Learn more
+            </div>
+            <HStack gap="2" alignItems="center">
+              <div>Governance handbook</div>
+              <ArrowTopRightOnSquareIcon
+                className={css`
+                  width: 16px;
+                  height: 16px;
+                  color: ${theme.colors.gray["500"]};
+                `}
+              ></ArrowTopRightOnSquareIcon>
+            </HStack>
+          </VStack>
+        </HStack>
+      </a>
 
-      <MetricContainer
-        icon="ballot"
-        title="Quorum"
-        body={
-          <>
-            <TokenAmountDisplay fragment={metrics.quorum.amount} /> (
-            {bpsToString(metrics.quorum.bpsOfTotal)} of supply)
-          </>
-        }
-      />
-
-      <MetricContainer
-        icon="measure"
-        title="Proposal threshold"
-        body={
-          <>
-            <TokenAmountDisplay fragment={metrics.proposalThreshold.amount} /> (
-            {bpsToString(metrics.proposalThreshold.bpsOfTotal)} of supply)
-          </>
-        }
-      />
+      <a
+        href="https://calendar.google.com/calendar/u/0/r?cid=Y19mbm10Z3VoNm5vbzZxZ2JuaTJncGVyaWQ0a0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <HStack
+          gap="3"
+          className={css`
+            background: ${theme.colors.white};
+            border-radius: ${theme.spacing["3"]};
+            padding: ${theme.spacing["3"]};
+            border-width: ${theme.spacing.px};
+            border-color: ${theme.colors.gray["300"]};
+            box-shadow: ${theme.boxShadow.newDefault};
+          `}
+        >
+          <div
+            className={css`
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              border-radius: ${theme.spacing["2"]};
+              border-width: ${theme.spacing.px};
+              border-color: ${theme.colors.gray["300"]};
+              background: ${color};
+              flex-shrink: 0;
+              padding: ${theme.spacing["3"]};
+            `}
+          >
+            <CalendarIcon
+              className={css`
+                color: ${theme.colors.gray["700"]};
+                width: 24px;
+                height: 24px;
+              `}
+            ></CalendarIcon>
+          </div>
+          <VStack>
+            <div
+              className={css`
+                font-size: ${theme.fontSize.sm};
+                color: ${theme.colors.gray["700"]};
+              `}
+            >
+              Learn more
+            </div>
+            <HStack gap="2" alignItems="center">
+              <div>Proposals calendar</div>
+              <ArrowTopRightOnSquareIcon
+                className={css`
+                  width: 16px;
+                  height: 16px;
+                  color: ${theme.colors.gray["500"]};
+                `}
+              ></ArrowTopRightOnSquareIcon>
+            </HStack>
+          </VStack>
+        </HStack>
+      </a>
     </HStack>
   );
 }
