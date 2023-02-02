a4fb3ff05bc9d81087c5fe7aa09169cff6f08dce
martin <martincharles07@gmail.com>
2023-02-02 00:59:36 -0600

    add lmdb implementation
    maybe this will fix the leveldb stuff

---
+++ packages/backend/src/indexer/storage/lmdb/lmdbReader.ts
@@ -0,0 +1,104 @@
+import {
+  EntityDefinitions,
+  getEntitiesByIndexFromStorageArea,
+  getEntityFromStorageArea,
+  IndexQueryArgs,
+  Reader,
+} from "../reader";
+import { StorageArea } from "../../followChain";
+import { RuntimeType } from "../../serde";
+import { LmdbEntityStore } from "./lmdbEntityStore";
+
+export class LmdbReader<EntityDefinitionsType extends EntityDefinitions>
+  implements Reader<EntityDefinitionsType>
+{
+  readonly entityDefinitions: EntityDefinitionsType;
+
+  private readonly storageArea: StorageArea;
+
+  private readonly entityStore: LmdbEntityStore;
+
+  constructor(
+    entityDefinitions: EntityDefinitionsType,
+    entityStore: LmdbEntityStore,
+    storageArea: StorageArea
+  ) {
+    this.entityDefinitions = entityDefinitions;
+    this.entityStore = entityStore;
+    this.storageArea = storageArea;
+  }
+
+  getEntitiesByIndex<
+    Entity extends keyof EntityDefinitionsType & string,
+    IndexName extends EntityDefinitionsType[Entity]["indexes"][0]["indexName"]
+  >(
+    entity: Entity,
+    indexName: IndexName,
+    args: IndexQueryArgs
+  ): AsyncGenerator<
+    Readonly<RuntimeType<EntityDefinitionsType[Entity]["serde"]>>
+  > {
+    const entityDefinition = this.entityDefinitions[entity];
+    const lmdb = this.entityStore.lmdb;
+
+    return getEntitiesByIndexFromStorageArea(
+      entity,
+      entityDefinition,
+      indexName,
+      args,
+      this.storageArea,
+      async function* (indexPrefix, startingKey, visitedValues) {
+        for (const { key, value: entityId } of lmdb.getRange({
+          start: startingKey,
+        })) {
+          const indexKey = key as string;
+          if (!indexKey.startsWith(indexPrefix)) {
+            break;
+          }
+
+          if (visitedValues.has(indexKey)) {
+            continue;
+          }
+
+          const rawValue = lmdb.get(entityId);
+          if (!rawValue) {
+            throw new Error("index value not found");
+          }
+
+          const value = entityDefinition.serde.deserialize(rawValue);
+
+          yield {
+            entityId,
+            indexKey,
+            value,
+          };
+        }
+      }
+    );
+  }
+
+  async getEntity<Entity extends keyof EntityDefinitionsType & string>(
+    entity: Entity,
+    id: string
+  ): Promise<Readonly<
+    RuntimeType<EntityDefinitionsType[Entity]["serde"]>
+  > | null> {
+    const entityDefinition = this.entityDefinitions[entity];
+    const fromStorage = getEntityFromStorageArea(
+      this.storageArea,
+      entity,
+      entityDefinition,
+      id
+    );
+    if (fromStorage) {
+      return fromStorage.value;
+    }
+
+    const fromStore = await this.entityStore.getEntity(entity, id);
+    if (!fromStore) {
+      return null;
+    }
+
+    return entityDefinition.serde.deserialize(fromStore);
+  }
+}
