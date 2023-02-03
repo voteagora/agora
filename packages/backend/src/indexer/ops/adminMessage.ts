26b39e408cb0dad23250aa931f8617c1c3ed7c17
martin <martincharles07@gmail.com>
2023-02-02 21:55:04 -0600

    add error logging
    some of these values are too large it seems ugh

--- packages/backend/src/indexer/ops/adminMessage.ts
+++ packages/backend/src/indexer/ops/adminMessage.ts
@@ -15,10 +15,20 @@ export type AdminMessage =
 export async function sendAdminMessage(message: AdminMessage) {
   while (true) {
     try {
-      await fetch("https://optimism-agora-dev.act.workers.dev/admin/ops", {
-        method: "POST",
-        body: JSON.stringify(message),
-      });
+      const response = await fetch(
+        "https://optimism-agora-dev.act.workers.dev/admin/ops",
+        {
+          method: "POST",
+          body: JSON.stringify(message),
+        }
+      );
+      if (response.status !== 200) {
+        throw new Error(
+          `non-200 response ${response.status} ${
+            response.statusText
+          } ${await response.text()}`
+        );
+      }
       return;
     } catch (e) {
       console.error(e);
