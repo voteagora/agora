3df275c2fcd2a2d4426e8794a9dcf996b63e8662
martin <martincharles07@gmail.com>
2023-02-06 14:16:14 -0600

    use loader for generate-schema too
    turn off transpileOnly too

--- packages/backend/scripts/registerGraphQL.ts
+++ packages/backend/scripts/registerGraphQL.ts
@@ -1,5 +1,8 @@
+// @ts-ignore
 import * as fs from "fs";
 
+// @ts-ignore
 require.extensions[".graphql"] = function (module, filename) {
+  // @ts-ignore
   module.exports = fs.readFileSync(filename, "utf-8");
 };
