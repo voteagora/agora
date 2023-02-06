677c490e18ae044916a2ea3e6d3d5eddfa4eaced
martin <martincharles07@gmail.com>
2023-02-06 12:32:37 -0600

    store snapshot data by path
    this allows for switching branches

--- packages/backend/src/bin/scripts/loadSnapshotVotes/run.ts
+++ packages/backend/src/bin/scripts/loadSnapshotVotes/run.ts
@@ -2,22 +2,27 @@ import { getAllFromQuery, proposalsQuery, votesQuery } from "./queries";
 import { promises as fs } from "fs";
 import { loadJsonLines } from "../../../utils/jsonLines";
 import { takeLast } from "../../../indexer/utils/generatorUtils";
+import path from "path";
 
 const spaceId = "opcollective.eth";
 
 async function main() {
-  const { path, query } = (() => {
+  const basePath = `data/snapshot/${spaceId}`;
+
+  await fs.mkdir(basePath, { recursive: true });
+
+  const { file, query } = (() => {
     switch (process.argv[2]) {
       case "votes": {
         return {
-          path: "data/snapshot/votes.jsonl",
+          file: path.join(basePath, `votes.jsonl`),
           query: votesQuery,
         };
       }
 
       case "proposals": {
         return {
-          path: "data/snapshot/proposals.jsonl",
+          file: path.join(basePath, "proposals.jsonl"),
           query: proposalsQuery,
         };
       }
@@ -28,9 +33,9 @@ async function main() {
     }
   })();
 
-  const lastEntry = await takeLast(loadJsonLines<{ created: number }>(path));
+  const lastEntry = await takeLast(loadJsonLines<{ created: number }>(file));
 
-  const votesFile = await fs.open(path, "a+");
+  const votesFile = await fs.open(file, "a+");
   for await (const vote of (async function* () {
     for await (const voteBatch of getAllFromQuery(
       query as any,
