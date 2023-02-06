45c71b36b1823bd105d30a6b96aba0dc58a1975c
martin <martincharles07@gmail.com>
2023-02-06 01:02:21 -0600

    use fast-check/jest

--- packages/backend/src/indexer/utils/efficientLengthEncoding.test.ts
+++ packages/backend/src/indexer/utils/efficientLengthEncoding.test.ts
@@ -1,33 +1,30 @@
-import fc from "fast-check";
+import { it, fc } from "@fast-check/jest";
+
 import { efficientLengthEncodingNaturalNumbers } from "./efficientLengthEncoding";
 import { ethers } from "ethers";
 import { compareBy } from "./sortUtils";
 
 describe("efficientLengthEncoding", () => {
-  it("sorts numbers correctly", () => {
-    fc.assert(
-      fc.property(fc.array(fc.integer({})), (numbers) => {
-        const values = numbers.map((number, idx) => ({ number, idx }));
-        const referenceSortedValues = values
-          .slice()
-          .sort(compareBy((it) => it.number))
-          .map((it) => it.idx);
+  it.prop([fc.array(fc.integer())])("sorts numbers correctly", (numbers) => {
+    const values = numbers.map((number, idx) => ({ number, idx }));
+    const referenceSortedValues = values
+      .slice()
+      .sort(compareBy((it) => it.number))
+      .map((it) => it.idx);
 
-        const encodedSort = values
-          .slice()
-          .map((item) => ({
-            number: item.number,
-            idx: item.idx,
-            encoded: efficientLengthEncodingNaturalNumbers(
-              ethers.BigNumber.from(item.number)
-            ),
-          }))
-          .sort(compareBy((it) => it.encoded));
+    const encodedSort = values
+      .slice()
+      .map((item) => ({
+        number: item.number,
+        idx: item.idx,
+        encoded: efficientLengthEncodingNaturalNumbers(
+          ethers.BigNumber.from(item.number)
+        ),
+      }))
+      .sort(compareBy((it) => it.encoded));
 
-        const encodedValues = encodedSort.map((it) => it.idx);
+    const encodedValues = encodedSort.map((it) => it.idx);
 
-        expect(encodedValues).toEqual(referenceSortedValues);
-      })
-    );
+    expect(encodedValues).toEqual(referenceSortedValues);
   });
 });
