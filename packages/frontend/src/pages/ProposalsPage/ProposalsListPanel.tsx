e726d8ed5f5c6a27c694b01643ab6531133fe730
martin <martincharles07@gmail.com>
2023-02-08 13:40:15 -0600

    fix filter

--- packages/frontend/src/pages/ProposalsPage/ProposalsListPanel.tsx
+++ packages/frontend/src/pages/ProposalsPage/ProposalsListPanel.tsx
@@ -150,7 +150,7 @@ export function ProposalsListPanel({
                 },
                 {
                   title: "Defeated",
-                  value: "VETOED" as const,
+                  value: "DEFEATED" as const,
                 },
                 {
                   title: "Queued",
