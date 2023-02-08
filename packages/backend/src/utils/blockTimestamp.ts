ad5f3a8c3078bd88b6ebebf62a6f4b7ae79f1f63
martin <martincharles07@gmail.com>
2023-02-07 21:07:47 -0600

    change block timestamp to date

--- packages/backend/src/utils/blockTimestamp.ts
+++ packages/backend/src/utils/blockTimestamp.ts
@@ -5,14 +5,16 @@ const secondsPerBlock = 12;
 export async function approximateBlockTimestampForBlock(
   provider: ethers.providers.BaseProvider,
   blockNumber: number
-) {
+): Promise<Date> {
   const block = await provider.getBlock(blockNumber.toString());
   if (block) {
-    return block.timestamp;
+    return new Date(block.timestamp * 1000);
   }
 
   const latestBlock = await provider.getBlock("latest");
   const blockDelta = blockNumber - latestBlock.number;
 
-  return latestBlock.timestamp + blockDelta * secondsPerBlock;
+  return new Date(
+    (latestBlock.timestamp + blockDelta * secondsPerBlock) * 1000
+  );
 }
