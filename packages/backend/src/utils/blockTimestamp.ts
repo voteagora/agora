0b6a9eeafb03b1facc47cc3d9e74ca2f78653913
martin <martincharles07@gmail.com>
2023-02-09 01:50:24 -0600

    fixed block timestamp parameter

--- packages/backend/src/utils/blockTimestamp.ts
+++ packages/backend/src/utils/blockTimestamp.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 
-const secondsPerBlock = 12;
+const secondsPerBlock = 0.25;
 
 export async function approximateBlockTimestampForBlock(
   provider: ethers.providers.BaseProvider,
