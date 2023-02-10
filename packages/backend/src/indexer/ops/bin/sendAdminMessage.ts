8f78814e58629128e60bc9d918985683d54e4665
martin <martincharles07@gmail.com>
2023-02-10 14:48:06 -0600

    admin stuff
    do not deploy any of this

---
+++ packages/backend/src/indexer/ops/bin/sendAdminMessage.ts
@@ -0,0 +1,21 @@
+import "isomorphic-fetch";
+import { sendAdminMessage } from "../adminMessage";
+
+export async function main() {
+  await sendAdminMessage({
+    type: (() => {
+      const command = process.argv[2];
+      switch (command) {
+        case "START":
+        case "STOP":
+        case "STEP":
+          return command;
+
+        default:
+          throw new Error(`invalid command ${command}`);
+      }
+    })(),
+  });
+}
+
+main();
