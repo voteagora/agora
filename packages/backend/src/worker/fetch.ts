cdec9e32f477046932c415d31d67b8741c01f6e6
martin <martincharles07@gmail.com>
2023-02-14 21:54:17 -0600

    Fix Type Imports

--- packages/backend/src/worker/fetch.ts
+++ packages/backend/src/worker/fetch.ts
@@ -1,6 +1,6 @@
 import { Env } from "./env";
 import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-import { Request, Response } from "@cloudflare/workers-types";
+import type { Request, Response } from "@cloudflare/workers-types";
 
 import manifestJSON from "__STATIC_CONTENT_MANIFEST";
 const assetManifest = JSON.parse(manifestJSON);
