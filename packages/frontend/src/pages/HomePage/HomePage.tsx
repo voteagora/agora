5d2bc502f9ca534d243a963033a524c97ad078db
Yitong Zhang <zha.yitong@gmail.com>
2023-03-29 10:46:25 -0700

    linter

--- packages/frontend/src/pages/HomePage/HomePage.tsx
+++ packages/frontend/src/pages/HomePage/HomePage.tsx
@@ -145,56 +145,56 @@ function Banner() {
   );
 }
 
-function Hero() {
-  return (
-    <VStack
-      className={css`
-        max-width: ${theme.maxWidth["xl"]};
-        text-align: center;
-        padding: 0 ${theme.spacing["4"]};
-        margin: ${theme.spacing["16"]} 0;
-        @media (max-width: ${theme.maxWidth.lg}) {
-          margin: 0;
-          text-align: left;
-          width: 100%;
-        }
-      `}
-    >
-      <h1
-        className={css`
-          font-weight: ${theme.fontWeight.extrabold};
-          font-size: ${theme.fontSize["2xl"]};
-          @media (min-width: ${theme.maxWidth.lg}) {
-            display: none;
-          }
-        `}
-      >
-        Voter metrics
-      </h1>
-      <h1
-        className={css`
-          font-weight: ${theme.fontWeight.extrabold};
-          font-size: ${theme.fontSize["2xl"]};
-          @media (max-width: ${theme.maxWidth.lg}) {
-            display: none;
-          }
-        `}
-      >
-        Agora is the home of nouns voters
-      </h1>
+// function Hero() {
+//   return (
+//     <VStack
+//       className={css`
+//         max-width: ${theme.maxWidth["xl"]};
+//         text-align: center;
+//         padding: 0 ${theme.spacing["4"]};
+//         margin: ${theme.spacing["16"]} 0;
+//         @media (max-width: ${theme.maxWidth.lg}) {
+//           margin: 0;
+//           text-align: left;
+//           width: 100%;
+//         }
+//       `}
+//     >
+//       <h1
+//         className={css`
+//           font-weight: ${theme.fontWeight.extrabold};
+//           font-size: ${theme.fontSize["2xl"]};
+//           @media (min-width: ${theme.maxWidth.lg}) {
+//             display: none;
+//           }
+//         `}
+//       >
+//         Voter metrics
+//       </h1>
+//       <h1
+//         className={css`
+//           font-weight: ${theme.fontWeight.extrabold};
+//           font-size: ${theme.fontSize["2xl"]};
+//           @media (max-width: ${theme.maxWidth.lg}) {
+//             display: none;
+//           }
+//         `}
+//       >
+//         Agora is the home of nouns voters
+//       </h1>
 
-      <p
-        className={css`
-          color: ${theme.colors.gray["700"]};
-          font-size: ${theme.fontSize.base};
-          @media (max-width: ${theme.maxWidth.lg}) {
-            display: none;
-          }
-        `}
-      >
-        Nouns voters are the stewards for the DAO. You can see them all below,
-        delegate your votes to them, or contact them about your ideas.
-      </p>
-    </VStack>
-  );
-}
+//       <p
+//         className={css`
+//           color: ${theme.colors.gray["700"]};
+//           font-size: ${theme.fontSize.base};
+//           @media (max-width: ${theme.maxWidth.lg}) {
+//             display: none;
+//           }
+//         `}
+//       >
+//         Nouns voters are the stewards for the DAO. You can see them all below,
+//         delegate your votes to them, or contact them about your ideas.
+//       </p>
+//     </VStack>
+//   );
+// }
