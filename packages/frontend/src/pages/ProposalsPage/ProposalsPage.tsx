1398e31fa8eb209d31dad7cce8589b15a4caa20c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-10 21:20:30 +0200

    fix: remove comment

--- packages/frontend/src/pages/ProposalsPage/ProposalsPage.tsx
+++ packages/frontend/src/pages/ProposalsPage/ProposalsPage.tsx
@@ -136,7 +136,6 @@ export function ProposalsPage() {
             queryFragmentRef={result}
             expanded={proposalsListExpanded}
           />
-          {/* <ProposalsAIPanel fragmentRef={result.firstProposal} /> */}
         </VStack>
       </HStack>
     </motion.div>
