41910a39ffee51ab20e4ac9c3705eab10755657f
Martin Charles <martincharles07@gmail.com>
2023-04-28 18:15:20 -0400

    add a nop implementation of the name resolver (#188)

--- packages/backend/src/shared/schema/context/nameResolver.ts
+++ packages/backend/src/shared/schema/context/nameResolver.ts
@@ -1,6 +1,32 @@
 import { Address } from "viem";
+import { ethers } from "ethers";
 
 export type NameResolver = {
-  resolveAddress(name: string): Promise<Address | null>;
+  resolveAddress(nameOrAddress: string): Promise<Address | null>;
   resolveNameFromAddress(address: Address): Promise<string | null>;
 };
+
+/**
+ * A name resolver which does not attempt to lookup names externally. When this
+ * resolver is used the address fallback is displayed instead of names
+ * everywhere.
+ *
+ * This is useful when:
+ * * debugging perf issues around ens name resolutions
+ * * using the hardhat simulated chain from a forked chain as name resolutions
+ *   are very slow there
+ */
+export function makeNopNameResolver(): NameResolver {
+  return {
+    async resolveAddress(nameOrAddress: string): Promise<Address | null> {
+      if (ethers.utils.isHexString(nameOrAddress)) {
+        return ethers.utils.getAddress(nameOrAddress) as Address;
+      }
+
+      return null;
+    },
+    async resolveNameFromAddress(address: Address): Promise<string | null> {
+      return null;
+    },
+  };
+}
