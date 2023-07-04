a53e5088d151091fe8a00c961deefa5fd44a5c99
Charlie <charliecf@users.noreply.github.com>
2023-07-03 20:50:00 -0400

    added twitter and discord link outs on pages (#275)

--- packages/frontend/src/components/PageHeader.tsx
+++ packages/frontend/src/components/PageHeader.tsx
@@ -120,6 +120,7 @@ export function PageHeader() {
           height: ${theme.spacing["6"]};
         `}
       >
+        <SocialLinks />
         <ConnectWalletButton />
       </HStack>
     </HStack>
@@ -238,5 +239,51 @@ export const MobileButton = () => {
         );
       }}
     </ConnectKitButton.Custom>
+  );
+};
+
+export const SocialLinks = () => {
+  const isMobile = useMediaQuery({
+    query: `(max-width: ${theme.maxWidth.md})`,
+  });
+
+  const socialLinks = [
+    {
+      icon: icons.discord,
+      alt: "discord",
+      url: "https://discord.gg/dM2ZhvvZ", // Replace with your Discord URL
+    },
+    {
+      icon: icons.twitter,
+      alt: "twitter",
+      url: "https://twitter.com/nounsagora", // Replace with your Twitter URL
+    },
+  ];
+
+  if (isMobile) {
+    return null;
+  }
+
+  return (
+    <HStack gap="3" alignItems="center">
+      {socialLinks.map(({ icon, alt, url }, index) => (
+        <a
+          key={index}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={css`
+            img {
+              margin-top: 22px;
+              height: ${theme.spacing["4"]};
+              width: ${theme.spacing["4"]};
+              vertical-align: middle;
+            }
+          `}
+        >
+          <img src={icon} alt={alt} />
+        </a>
+      ))}
+    </HStack>
   );
 };
